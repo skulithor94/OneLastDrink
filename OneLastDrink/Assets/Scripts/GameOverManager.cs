9a90872878bfdeafe15e274a228b3b6b252e2c8d
Skuli <skuliarnas@gmail.com>
2016-05-03 20:03:19 +0000

    started making lose conditions

---
+++ OneLastDrink/Assets/Scripts/GameOverManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameOverManager : MonoBehaviour {
+
+	//Two seconds given so the scene doesn't load immediately
+	public float gameOverDelay = 2f;
+
+	bool gameOverCheck = false;
+	float gameOverTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameOverCheck) {
+
+			gameOverTimer += Time.deltaTime;
+
+			if (gameOverTimer >= gameOverDelay) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+			}
+		}
+	}
+
+	public void gameOver(){
+		gameOverCheck = true;
+	}
+}
--- OneLastDrink/Assets/Scripts/Progress.cs
+++ OneLastDrink/Assets/Scripts/Progress.cs
@@ -2,23 +2,30 @@
 using UnityEngine.UI;
 using System.Collections;
 
+
+
 public class Progress : MonoBehaviour {
 
 	//Gets the flashlight image and the speed at which the progress bar fills
 	//could be changed to seconds later
 	public Image flashlight;
 	public float flashlightSpeed;
+	public GameOverManager gameOverManager;
+	public GameObject player;
 
 	// Use this for initialization
 	void Start () {
-	
+		//There must be a better way..
+		gameOverManager = FindObjectOfType (typeof(GameOverManager)) as GameOverManager;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		//If the image's fill amount is 0 the game ends, if not the flashlight fill amount decreases.
 		if (flashlight.fillAmount <= 0f) {
-			Debug.Log ("Game Over!");
+			//Destroy the current player so that the lights go out.
+			Destroy (player);
+			gameOverManager.gameOver ();
 		}else{	
 			flashlight.fillAmount -= flashlightSpeed * Time.deltaTime;
 		}			

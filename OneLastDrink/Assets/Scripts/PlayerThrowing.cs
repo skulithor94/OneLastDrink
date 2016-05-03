29652a288f35647c104d126f9353c4d00c28d4ba
Kristinn Guðmundsson <kristinngudmundsson@clients-pool-1.nat.ru.is>
2016-05-03 15:19:52 +0000

    throwing pills

---
+++ OneLastDrink/Assets/Scripts/PlayerThrowing.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerThrowing : MonoBehaviour {
+
+	public GameObject pillBoxPrefab;
+	public float pillBoxCount = 1;
+	public float fireDelay = 0.5f;
+	float cooldownTimer = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		cooldownTimer -= Time.deltaTime;
+		if (Input.GetButton("Fire1") && cooldownTimer <= 0 && 0 < pillBoxCount ) {
+			Debug.Log ("wow");
+			cooldownTimer = fireDelay;
+			Instantiate (pillBoxPrefab, transform.position, transform.rotation);
+			pillBoxCount -= 1;
+		}	
+	}
+}

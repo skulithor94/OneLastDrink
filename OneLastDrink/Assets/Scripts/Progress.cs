d3d2f5908ded406d3cfb79a551d295f1c05fb356
ingthorb@gmail.com <ingthorb@gmail.com>
2016-05-05 14:42:55 +0000

    still working on warning

--- OneLastDrink/Assets/Scripts/Progress.cs
+++ OneLastDrink/Assets/Scripts/Progress.cs
@@ -10,19 +10,36 @@ public class Progress : MonoBehaviour {
 	//could be changed to seconds later
 	public Image flashlight;
 	public float flashlightSpeed;
+    private bool hasColorChanged;
+    public Image foregroundColor;
 
-	GameOverManager gameOverManager;
+    GameOverManager gameOverManager;
 
 	// Use this for initialization
 	void Start () {
 		//Allows for interaction with gameOverManager functions
 		gameOverManager = GameObject.FindGameObjectWithTag("GameOverManager").GetComponent<GameOverManager>();
-	}
+        hasColorChanged = false;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		//If the image's fill amount is 0 the game ends, if not the flashlight fill amount decreases.
-		if (flashlight.fillAmount <= 0f) {
+        /*if (flashlight.fillAmount < 0.4f & flashlight.fillAmount > 0f)
+        {
+            if (hasColorChanged == false)
+            {
+                Debug.Log("Time is running out");
+                foregroundColor = GameObject.Find("Foreground").GetComponent<Image>();
+
+                // Color red = new Color(255, 9, 0, 1);
+                //foregroundColor.material.color = Color.red;
+
+                Debug.Log(flashlight);
+                hasColorChanged = true;
+            }
+        }*/
+        //If the image's fill amount is 0 the game ends, if not the flashlight fill amount decreases.
+        if (flashlight.fillAmount <= 0f) {
 			gameOverManager.gameOver ();
 		}else{	
 			flashlight.fillAmount -= flashlightSpeed * Time.deltaTime;

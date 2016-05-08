664117aeffa3d6568f639f2706ee81405275ecf7
Skuli <skuliarnas@gmail.com>
2016-05-08 11:24:37 +0000

    changed camera

--- OneLastDrink/Assets/Scripts/CameraControl.cs
+++ OneLastDrink/Assets/Scripts/CameraControl.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class CameraControl : MonoBehaviour {
 
 	public Transform player;
+	public float smooting = 5f;
+
+	Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-	
+		offset = transform.position - player.position;
+		transform.position = player.position;
 	}
 	
 	// Update is called once per frame
@@ -15,7 +19,9 @@ public class CameraControl : MonoBehaviour {
 
 		//Used to match the position of the camera to the position of the player.
 		if (player != null) {
-			transform.position = new Vector3 (player.position.x, player.position.y, transform.position.z);
+			Vector3 targetCamPos = player.position + offset;
+			transform.position = Vector3.Lerp (transform.position, targetCamPos, smooting * Time.deltaTime);
+		//	transform.position = new Vector3 (player.position.x, player.position.y, transform.position.z);
 		}
 	}
 }

82fcaf49e4d786361fd952d81eac56000fc6366f
Skuli <skuliarnas@gmail.com>
2016-05-10 11:19:30 +0000

    fixed sound slider bug

--- OneLastDrink/Assets/Scripts/PauseManager.cs
+++ OneLastDrink/Assets/Scripts/PauseManager.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PauseManager : MonoBehaviour {
 
 	private Player player;
 	private PlayerThrowing playerThrow;
+	private Slider[] sliders;
 	public GameObject panel;
 	public AudioMixer masterMixer;
 
@@ -14,6 +16,9 @@ public class PauseManager : MonoBehaviour {
 		player = GameObject.FindGameObjectWithTag ("Player").GetComponent<Player>();
 		playerThrow = GameObject.FindGameObjectWithTag ("Player").GetComponent<PlayerThrowing> ();
 		panel.SetActive (false);
+
+		sliders = panel.GetComponentsInChildren<Slider> ();
+		InitializeVolume ();
 	}
 	
 	// Update is called once per frame
@@ -27,20 +32,22 @@ public class PauseManager : MonoBehaviour {
 		}
 	}
 
+	//Pause the game, need to pause the player so what lookrotation doesn't change.
+	//The playerThrow needs to be paused so that the player can't throw pills while the game is paused.
 	public void Pause(){
 		Time.timeScale = 0;
 		player.setPlayerPause (true);
 		playerThrow.setPlayerPause (true);
 		panel.SetActive (true);
 	}
-
+		
 	public void Unpause(){
 		Time.timeScale = 1;
 		player.setPlayerPause (false);	
 		playerThrow.setPlayerPause (false);
 		panel.SetActive (false);
 	}
-
+		
 	public void SetSfxVol(float sfxLvl){
 		masterMixer.SetFloat ("effectsVol", sfxLvl);
 	}
@@ -51,5 +58,19 @@ public class PauseManager : MonoBehaviour {
 
 	public void SetMasterVol(float masterLvl){
 		masterMixer.SetFloat ("masterVol", masterLvl);
+	}
+
+	//Initialize the sliders in the pause menu based on the values of the mixer.
+	private void InitializeVolume(){
+		float vol;
+
+		masterMixer.GetFloat ("effectsVol", out vol);
+		sliders [0].value = vol;
+
+		masterMixer.GetFloat ("masterVol", out vol);
+		sliders [1].value = vol;
+
+		masterMixer.GetFloat ("musicVol", out vol);
+		sliders [2].value = vol;	
 	}
 }

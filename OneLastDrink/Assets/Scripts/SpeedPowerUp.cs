55dfc1bf5ae192fa4992f22da4cbfba7941c817c
Skuli <skuliarnas@gmail.com>
2016-05-10 17:08:04 +0000

    speed powerup

---
+++ OneLastDrink/Assets/Scripts/SpeedPowerUp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeedPowerUp : MonoBehaviour {
+
+	private Player player;
+	private float duration = 3f;
+	private float speedBoost = 10f;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player").GetComponent<Player> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll){
+		if (coll.collider.tag == "Player") {
+			StartCoroutine (SpeedUp ());
+		}
+	}
+
+	IEnumerator SpeedUp(){
+		Destroy(gameObject.GetComponent<BoxCollider2D> ());
+		gameObject.GetComponent<SpriteRenderer> ().color = new Color(0f, 0f, 0f, 0f);
+		player.Speed += speedBoost;
+		yield return new WaitForSeconds (duration);
+		player.Speed -= speedBoost;
+		Destroy (gameObject);
+	}
+}

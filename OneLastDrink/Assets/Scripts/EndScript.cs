e1b2115182d4032a5f5fc7c7945c8f818e624ad7
Skuli <skuliarnas@gmail.com>
2016-05-16 13:55:58 +0000

    end and begining done

---
+++ OneLastDrink/Assets/Scripts/EndScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class EndScript : MonoBehaviour {
+
+	public void LoadLevel()
+	{
+		//Scene
+		SceneManager.LoadScene("MainMenu");
+	}
+}
